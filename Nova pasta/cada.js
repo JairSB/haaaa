4b57c054763629153cae86f6c781387a5170b9a1
JairSB <benarrozjair@gmail.com>
2024-10-04 21:41:33 -0300

    Add files via upload

--- Nova pasta/cada.js
+++ Nova pasta/cada.js
@@ -1,66 +1,108 @@
-const url_lis= "https://devup-tmp02-default-rtdb.firebaseio.com/";
-let text =[]
+const url_lis = "https://devup-tmp02-default-rtdb.firebaseio.com/";
 
-btn_cadastro.addEventListener('click',e=>{
-
-    let user={
-        'nome':nome.value,
-        'telefone':telefone.value,
-        'email':email.value
+btn_cadastro.addEventListener('click', e => {
+    let user = {
+        'nome': nome.value,
+        'telefone': telefone.value,
+        'email': email.value
     }
-    fetch(url_lis+'usuario.json',{
-        method:'POST',
-        body: JSON.stringify(user)
-    })
-    .then(resposta =>resposta.json())
-    .then(dados=>{
-        listar();
-    })
+    if (controle.value == "") {
+        fetch(url_lis + 'usuario.json', {
+            method: 'POST',
+            body: JSON.stringify(user)
+        })
+            .then(resposta => resposta.json())
+            .then(dados => {
+                frm.reset();
+                listar();
+            })
+    } else {
+        fetch(url_lis + 'usuario/' + controle.value + '.json', {
+            method: 'PUT', body: JSON.stringify(user)
+        }
+        )
+            .then(resposta => resposta.json)
+            .then(dados => {
+                listar();
+                frm.reset();
+                controle.value = "";
+                btn_cadastro.classList.remove("btn-secondary");
+                btn_cadastro.classList.add("btn-success");
+                btn_cadastro.innerHTML = 'Cadstrar';
+            })
+
+    }
+
 
 })
 
 
 
-function listar(){
-    let lista=document.getElementById("lista");
-    fetch(url_lis+'usuario.json')
-    .then(resposta =>resposta.json())
-    .then(dados=>{
-        console.log(dados)
-        lista.innerHTML="";
-        for (const key in dados) {
-           lista.innerHTML+=`
+function listar() {
+    let lista = document.getElementById("lista");
+    fetch(url_lis + 'usuario.json')
+        .then(resposta => resposta.json())
+        .then(dados => {
+            console.log(dados)
+            lista.innerHTML = "";
+            for (const key in dados) {
+                lista.innerHTML += `
            <tr>
                 <td>${dados[key].nome}</td>
                 <td>${dados[key].telefone}</td>
                 <td>${dados[key].email}</td>
-                <button type="button" class="btn btn-danger" onclick="deleta(${key})">APAGER</button>
-                <button type="button" class="btn btn-warning" onclick="muda(${key})">ALTERAR</button>
+                <td>
+                <button type="button" class="btn btn-danger" onclick="deleta('${key}')">APAGER</button>
+                </td>
+                <td>
+                <button type="button" class="btn btn-warning" onclick="muda('${key}')">ALTERAR</button>
+                </td>
            </tr>
            `
-           console.log(key)
-        }
-    })
+            }
+        })
 }
-listar();
 
 
-function deleta(n){
-    fetch(url_lis+'usuario/'+n+'.json',{method:'DELETE'})
-    listar();
+function deleta(n) {
+    fetch(url_lis + 'usuario/' + n + '.json', { method: "DELETE" })
+    .then(resposta=>resposta.json())    
+    .then(dados=>(
+            listar()
+        )
+        )
+
 }
 
 function muda(n) {
     console.log(n)
     btn_cadastro.classList.add("btn-secondary");
     btn_cadastro.classList.remove("btn-success");
+    btn_cadastro.innerHTML = 'Modifica';
 
-    fetch(url_lis+'usuario.json')
-    .then(resposta =>resposta.json())
-    .then(dads=>{
+    fetch(url_lis + 'usuario/' + n + '.json')
+        .then(resposta => resposta.json())
+        .then(dads => {
 
-    nome.value=dads[n].nome
-    telefone.value=n.telefone
-    email.value=n.email
-})
+            nome.value = dads.nome;
+            telefone.value = dads.telefone;
+            email.value = dads.email;
+            controle.value = n;
+
+
+        })
+}
+
+setInterval(() => {
+    listar();
+}, 5000);
+
+
+listar();
+
+function expor(n){
+    var nome_arq=`${n}.xlsx`;
+    var exportacao=document.getElementById(tbe);
+    var plani = XLSX.utils.table_to_book(exportacao);
+    XLSX.writeFile(plani,nome_arq);
 }
